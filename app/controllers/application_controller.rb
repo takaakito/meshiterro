a2f8de486e8d0eeb0153519cf890a7a3ae790af4
EC2 Default User <ec2-user@ip-172-31-83-109.ec2.internal>
2022-03-18 07:26:26 +0000

    [add]validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+before_action :authenticate_user!, except: [:top]  
 before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
@@ -7,7 +8,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   
    def after_sign_out_path_for(resource)
     about_path
-  end
+   end
 
   protected
 
